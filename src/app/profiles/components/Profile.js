52ec1913cb93aa62ad0acb330b356727e045217e
Jorge Garcia <jegcelorio@yahoo.com>
2023-10-25 23:00:10 -0500

    Implemented Profile

---
+++ src/app/profiles/components/Profile.js
@@ -0,0 +1,16 @@
+import styles from '../styles/styles.module.css';
+
+export default function Profile() {
+    const avatar = "https://i.imgur.com/EdbYG2ls.jpeg";
+    const description = "Earthworm Jim"
+    return (
+        <>
+            
+            <img
+                className={styles.profile}
+                src={avatar}
+                alt={description}
+            />
+        </>
+    )
+}
