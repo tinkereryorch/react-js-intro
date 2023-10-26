dd3d09d68dca9c384167726a0068d78932bcff3a
Jorge Garcia <jegcelorio@yahoo.com>
2023-10-25 23:25:59 -0500

    Updated Gallery

--- src/app/profiles/page.js
+++ src/app/profiles/page.js
@@ -1,13 +1,9 @@
-import Profile from "./components/Profile";
+import TodoList from "./components/TodoList";
 
 export default function Gallery() {
-    const name = "Earthworm Jim";
     return(
         <section>
-            <h1>This is {name}</h1>
-            <Profile />
-            <Profile />
-            <Profile />
+            <TodoList />
         </section>
     )
 }
