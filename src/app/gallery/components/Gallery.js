d8b63fe91b4a5b0e75164c99ba5c9831dffc47b0
Jorge Garcia <jegcelorio@yahoo.com>
2023-10-26 16:38:50 -0500

    Added character info

--- src/app/gallery/components/Gallery.js
+++ src/app/gallery/components/Gallery.js
@@ -4,9 +4,9 @@ export default function Gallery() {
     return (
         <>
             <h1>This is the Gallery</h1>
-            <Profile />
-            <Profile />
-            <Profile />
+            <Profile name="Mario" avatar="https://mario.nintendo.com/static/86bd56fed456e9b642100519880b6a86/b3853/mario.png"/>
+            <Profile name="Bowser" avatar="https://upload.wikimedia.org/wikipedia/en/9/92/Bowser_Stock_Art_2021.png"/>
+            <Profile name="Peach" avatar="https://static.wikia.nocookie.net/supermariobrosfilm/images/d/d8/Princess_Peach_%28NEW%29.png"/>
         </>
     );
 }
