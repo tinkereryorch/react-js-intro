0269e9d987e814f205d66b70117497ea8c6cadf3
Jorge Garcia <jegcelorio@yahoo.com>
2023-10-26 15:31:06 -0500

    Imported Profile

--- src/app/gallery/components/Gallery.js
+++ src/app/gallery/components/Gallery.js
@@ -1,5 +1,12 @@
+import Profile from './Profile';
+
 export default function Gallery() {
     return (
-        <h1>This is the Gallery</h1>
+        <>
+            <h1>This is the Gallery</h1>
+            <Profile />
+            <Profile />
+            <Profile />
+        </>
     );
 }
